e610e1783494053eb84362cd6889c97f6fbb2fd0
Usmandhedhi <66262964+Usmandhedhi@users.noreply.github.com>
2020-06-19 17:51:52 +0000

    learning react dinner.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dinner from './Dinner.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world from Usman Amin </h1>   </div>
-  );
+     <Dinner dishName="Chicken Karahi" sweetDish="Gajar Ka Halwa"/>
+     <hr/>
+     <Dinner dishName="Biryani" sweetDish="Gulab Jamun"/>
+     <hr/>
+     <Dinner dishName="BBQ" sweetDish="Icecream"/>
+     
+       </div>
+  ); 
 }
 
 export default App;
